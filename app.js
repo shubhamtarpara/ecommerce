a1fc897861c3227f3c925f09a203dba93a0d2d5a
shubhamtarpara <shubham.t@zuru.tech>
2022-05-26 18:15:08 +0530

    cart functionality

--- app.js
+++ app.js
@@ -4,110 +4,109 @@ let carts = document.querySelectorAll('.btn-text');
 //   // Clear the local storage
 //   window.localStorage.clear();
 // }
-let products = [
-  {
-    name: 'Learn Python',
-    tag: 'Learn Python',
-    price: 54.99,
-    inCart: 0
-  },
-  {
-    name: 'Learn Python',
-    tag: 'Learn Python',
-    price: 54.99,
-    inCart: 0
-  },
-  {
-    name: 'Learn Python',
-    tag: 'Learn Python',
-    price: 54.99,
-    inCart: 0
-  },
-  {
-    name: 'Learn Python',
-    tag: 'Learn Python',
-    price: 54.99,
-    inCart: 0
-  },
-  {
-    name: 'Parlor Palm',
-    tag: 'Parlor Palm',
-    price: 74.99,
-    inCart: 0
-  },
-  {
-    name: 'Parlor Palm',
-    tag: 'Parlor Palm',
-    price: 74.99,
-    inCart: 0
-  },
-  {
-    name: 'Parlor Palm',
-    tag: 'Parlor Palm',
-    price: 74.99,
-    inCart: 0
-  },
-  {
-    name: 'Parlor Palm',
-    tag: 'Parlor Palm',
-    price: 74.99,
-    inCart: 0
-  },
-  {
-    name: 'Parlor Palm',
-    tag: 'Parlor Palm',
-    price: 74.99,
-    inCart: 0
-  },
-  {
-    name: 'Parlor Palm',
-    tag: 'Parlor Palm',
-    price: 74.99,
-    inCart: 0
-  },
-  {
-    name: 'Parlor Palm',
-    tag: 'Parlor Palm',
-    price: 74.99,
-    inCart: 0
-  },
-  {
-    name: 'Parlor Palm',
-    tag: 'Parlor Palm',
-    price: 74.99,
-    inCart: 0
-  },
-  {
-    name: 'Parlor Palm',
-    tag: 'Parlor Palm',
-    price: 74.99,
-    inCart: 0
-  },
-  {
-    name: 'Parlor Palm',
-    tag: 'Parlor Palm',
-    price: 74.99,
-    inCart: 0
-  },
-  {
-    name: 'Parlor Palm',
-    tag: 'Parlor Palm',
-    price: 74.99,
-    inCart: 0
-  },
-  {
-    name: 'Parlor Palm',
-    tag: 'Parlor Palm',
-    price: 74.99,
-    inCart: 0
-  },
+let products = [{
+  name: 'Learn Python',
+  tag: 'learnpython',
+  price: 50,
+  inCart: 0
+},
+{
+  name: 'Learn Python',
+  tag: 'learnpython',
+  price: 50,
+  inCart: 0
+},
+{
+  name: 'Learn Python',
+  tag: 'learnpython',
+  price: 50,
+  inCart: 0
+},
+{
+  name: 'Learn Python',
+  tag: 'learnpython',
+  price: 50,
+  inCart: 0
+},
+{
+  name: 'Parlor Palm',
+  tag: 'parlorpalm',
+  price: 60,
+  inCart: 0
+},
+{
+  name: 'Parlor Palm',
+  tag: 'parlorpalm',
+  price: 60,
+  inCart: 0
+},
+{
+  name: 'Parlor Palm',
+  tag: 'parlorpalm',
+  price: 60,
+  inCart: 0
+},
+{
+  name: 'Parlor Palm',
+  tag: 'parlorpalm',
+  price: 60,
+  inCart: 0
+},
+{
+  name: 'Parlor Palm',
+  tag: 'parlorpalm',
+  price: 60,
+  inCart: 0
+},
+{
+  name: 'Parlor Palm',
+  tag: 'parlorpalm',
+  price: 60,
+  inCart: 0
+},
+{
+  name: 'Parlor Palm',
+  tag: 'parlorpalm',
+  price: 60,
+  inCart: 0
+},
+{
+  name: 'Parlor Palm',
+  tag: 'parlorpalm',
+  price: 60,
+  inCart: 0
+},
+{
+  name: 'Parlor Palm',
+  tag: 'parlorpalm',
+  price: 60,
+  inCart: 0
+},
+{
+  name: 'Parlor Palm',
+  tag: 'parlorpalm',
+  price: 60,
+  inCart: 0
+},
+{
+  name: 'Parlor Palm',
+  tag: 'parlorpalm',
+  price: 60,
+  inCart: 0
+},
+{
+  name: 'Parlor Palm',
+  tag: 'parlorpalm',
+  price: 60,
+  inCart: 0
+},
 ];
 
 
 for(let i=0; i< carts.length; i++) {
   carts[i].addEventListener('click', () => {
       cartNumbers(products[i]);
-      
+      totalCost(products[i]);
   });
 }
 
@@ -144,7 +143,7 @@ function setItems(product) {
   // productNumbers = parseInt(productNumbers);
   let cartItems = localStorage.getItem('productsInCart');
   cartItems = JSON.parse(cartItems);
-console.log(cartItems)
+
   if(cartItems != null) {
       let currentProduct = product.tag;
   
@@ -166,129 +165,186 @@ console.log(cartItems)
   localStorage.setItem('productsInCart', JSON.stringify(cartItems));
 }
 
+function totalCost( product, action ) {
+  let cart = localStorage.getItem("totalCost");
+
+  if( action) {
+      cart = parseInt(cart);
+
+      localStorage.setItem("totalCost", cart - product.price);
+  } else if(cart != null) {
+      
+      cart = parseInt(cart);
+      localStorage.setItem("totalCost", cart + product.price);
+  
+  } else {
+      localStorage.setItem("totalCost", product.price);
+  }
+}
+
+function displayCart() {
+  let cartItems = localStorage.getItem('productsInCart');
+  cartItems = JSON.parse(cartItems);
+
+  let cart = localStorage.getItem("totalCost");
+  cart = parseInt(cart);
+
+  let productContainer = document.querySelector('.products');
+  let mainCost = document.querySelector('.mainCost')
+  
+  if( cartItems && productContainer && mainCost ) {
+      productContainer.innerHTML = '';
+      mainCost.innerHTML = '';
+      Object.values(cartItems).map( (item, index) => {
+          productContainer.innerHTML += 
+          `<div class="product"><i class="fa-solid fa-circle-xmark close"></i></i><img src="./images/${item.tag}.png"/>
+              <span class="sm-hide">${item.name}</span>
+          </div>
+          <div class="price sm-hide">$${item.price},00</div>
+          <div class="quantity">
+          <i class="fa-solid fa-circle-left decrease"></i></ion-icon>
+
+                  <span>${item.inCart}</span>
+                  <i class="fa-solid fa-circle-right increase"></i></i> 
+          </div>
+          <div class="total">$${item.inCart * item.price},00</div>`;
+      });
+
+      mainCost.innerHTML += `
+          <div class="basketTotalContainer">
+              <h4 class="basketTotalTitle">Basket Total</h4>
+              <h4 class="basketTotal">$${cart},00</h4>
+          </div>`
+
+      deleteButtons();
+      manageQuantity();
+  }
+}
+function manageQuantity() {
+  let decreaseButtons = document.querySelectorAll('.decrease');
+  let increaseButtons = document.querySelectorAll('.increase');
+  let currentQuantity = 0;
+  let currentProduct = '';
+  let cartItems = localStorage.getItem('productsInCart');
+  cartItems = JSON.parse(cartItems);
+
+  for(let i=0; i < increaseButtons.length; i++) {
+      decreaseButtons[i].addEventListener('click', () => {
+          console.log(cartItems);
+          currentQuantity = decreaseButtons[i].parentElement.querySelector('span').textContent;
+          console.log(currentQuantity);
+          currentProduct = decreaseButtons[i].parentElement.previousElementSibling.previousElementSibling.querySelector('span').textContent.toLocaleLowerCase().replace(/ /g,'').trim();
+          console.log(currentProduct);
+
+          if( cartItems[currentProduct].inCart > 1 ) {
+              cartItems[currentProduct].inCart -= 1;
+              cartNumbers(cartItems[currentProduct], "decrease");
+              totalCost(cartItems[currentProduct], "decrease");
+              localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+              displayCart();
+          }
+      });
+
+      increaseButtons[i].addEventListener('click', () => {
+          console.log(cartItems);
+          currentQuantity = increaseButtons[i].parentElement.querySelector('span').textContent;
+          console.log(currentQuantity);
+          currentProduct = increaseButtons[i].parentElement.previousElementSibling.previousElementSibling.querySelector('span').textContent.toLocaleLowerCase().replace(/ /g,'').trim();
+          console.log(currentProduct);
+
+          cartItems[currentProduct].inCart += 1;
+          cartNumbers(cartItems[currentProduct]);
+          totalCost(cartItems[currentProduct]);
+          localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+          displayCart();
+      });
+  }
+}
+
+function manageQuantity() {
+  let decreaseButtons = document.querySelectorAll('.decrease');
+  let increaseButtons = document.querySelectorAll('.increase');
+  let currentQuantity = 0;
+  let currentProduct = '';
+  let cartItems = localStorage.getItem('productsInCart');
+  cartItems = JSON.parse(cartItems);
+
+  for(let i=0; i < increaseButtons.length; i++) {
+      decreaseButtons[i].addEventListener('click', () => {
+          // console.log(cartItems);
+          // currentQuantity = decreaseButtons[i].parentElement.querySelector('span').textContent;
+          // console.log(currentQuantity);
+          // currentProduct = decreaseButtons[i].parentElement.previousElementSibling.previousElementSibling.querySelector('span').textContent.toLocaleLowerCase().replace(/ /g,'').trim();
+          // console.log(currentProduct);
+
+          if( cartItems[currentProduct].inCart > 1 ) {
+              cartItems[currentProduct].inCart -= 1;
+              cartNumbers(cartItems[currentProduct], "decrease");
+              totalCost(cartItems[currentProduct], "decrease");
+              localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+              displayCart();
+          }
+      });
+
+      increaseButtons[i].addEventListener('click', () => {
+          // console.log(cartItems);
+          // currentQuantity = increaseButtons[i].parentElement.querySelector('span').textContent;
+          // console.log(currentQuantity);
+          // currentProduct = increaseButtons[i].parentElement.previousElementSibling.previousElementSibling.querySelector('span').textContent.toLocaleLowerCase().replace(/ /g,'').trim();
+          // console.log(currentProduct);
+
+          cartItems[currentProduct].inCart += 1;
+          cartNumbers(cartItems[currentProduct]);
+          totalCost(cartItems[currentProduct]);
+          localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+          displayCart();
+      });
+  }
+}
+
+function deleteButtons() {
+  let deleteButtons = document.querySelectorAll(".close");
+  let productNumbers = localStorage.getItem('cartNumbers');
+  let cartCost = localStorage.getItem("totalCost");
+  let cartItems = localStorage.getItem('productsInCart');
+  cartItems = JSON.parse(cartItems);
+  let productName;
+  // console.log(cartItems);
+
+  for(let i=0; i < deleteButtons.length; i++) {
+      deleteButtons[i].addEventListener('click', () => {
+          productName = deleteButtons[i].parentElement.textContent.toLocaleLowerCase().replace(/ /g,'').trim();
+         
+          // localStorage.setItem('cartNumbers', productNumbers - cartItems[productName].inCart);
+          // localStorage.setItem('totalCost', cartCost - ( cartItems[productName].price * cartItems[productName].inCart));
+
+          delete cartItems[productName];
+          localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+
+          displayCart();
+          onLoadCartNumbers();
+      })
+  }
+}
+
 onLoadCartNumbers();
+displayCart();
+
 // localStorage.clear();
 
 
 
-// let noti = document.querySelector('h1');
+
+
+// let notification = document.querySelector('h1');
 // let button = document.getElementsByClassName('btn-text');
 // for(but of button){
 //   but.addEventListener('click', (e)=>{
-//     let add = Number(noti.getAttribute('data-count') || 0);
-//     noti.setAttribute('data-count', add +1);
+//     let add = Number(notification.getAttribute('data-count') || 0);
+//     notification.setAttribute('data-count', add +1);
 //     console.log(add)
-//     noti.classList.add('zero')
+//     notification.classList.add('zero')
 
 //   })
 // }
-
-const fadeInUp = document.querySelector('.shop-title')
-
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      fadeInUp.classList.add('fadeInUp');
-      return;
-    }
-  });
-});
-observer.observe(document.querySelector('.shop-title'));
-
-const fadeIn = document.querySelector('.shop-product')
-
-const observer1 = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      fadeIn.classList.add('fadeInUp');
-      return;
-    }
-  });
-});
-observer1.observe(document.querySelector('.shop-product'));
-
-const fade = document.querySelector('.box-container')
-
-const observer2 = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      fade.classList.add('fadeInUp');
-      return;
-    }
-  });
-});
-observer2.observe(document.querySelector('.box-container'));
-
-const fade1 = document.querySelector('.brand-icon')
-
-const observer5 = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      fade1.classList.add('fadeInUp');
-      return;
-    }
-  });
-});
-observer5.observe(document.querySelector('.brand-icon'));
-
-const fadeLeft = document.querySelector('.first-collection')
-
-const observer3 = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      fadeLeft.classList.add('fadeInLeft');
-      return;
-    }
-  });
-});
-observer3.observe(document.querySelector('.first-collection'));
-
-const fadeRight = document.querySelector('.second-collection')
-
-const observer4 = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      fadeRight.classList.add('fadeInRight');
-      return;
-    }
-  });
-});
-observer4.observe(document.querySelector('.second-collection'));
-
-const fadeLeftText = document.querySelector('.bottom-text')
-
-const observer6 = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      fadeLeftText.classList.add('fadeInLeft');
-      return;
-    }
-  });
-});
-observer6.observe(document.querySelector('.bottom-text'));
-
-const fadeRightImg = document.querySelector('.bottom-img')
-
-const observer7 = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      fadeRightImg.classList.add('fadeInRight');
-      return;
-    }
-  });
-});
-observer7.observe(document.querySelector('.bottom-img'));
-
-const fadeDown = document.querySelector('.end-img-container')
-
-const observer8 = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      fadeDown.classList.add('fadeInUp');
-      return;
-    }
-  });
-});
-observer8.observe(document.querySelector('.end-img-container'));
-
 
